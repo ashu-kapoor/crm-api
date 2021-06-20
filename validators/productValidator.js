7d4e5fe778d5baa7061dc74e466fdbfd09b8f620
Ashutosh Kapoor <ashutoshkapor1234@gmail.com>
2021-06-20 19:50:08 +0530

    Implemented product controller

--- validators/productValidator.js
+++ validators/productValidator.js
@@ -1,3 +1,31 @@
+const _ = require("lodash");
+
 module.exports = (req, res, next) => {
+  const name = req.body.name;
+  const code = req.body.code;
+  const listPrice = req.body.listPrice;
+
+  _.forEach([{ name }, { code }, { listPrice }], (obj) => {
+    _.forOwn(obj, function (value, key) {
+      if (!value) {
+        const error = new Error();
+        error.apiErrorCode = 1600;
+        error.apiData = key;
+        throw error;
+      }
+
+      if (key === "listPrice" && !matchesPattern(value)) {
+        const error = new Error();
+        error.apiErrorCode = 1600;
+        error.apiData = key;
+        throw error;
+      }
+    });
+  });
   next();
 };
+
+function matchesPattern(val) {
+  const re = /^\d{1,2}\.\d{1,2}$/;
+  return re.test(String(val).toLowerCase());
+}
